3fee61797ee3f01e7cd415af3e640ab69fed875c
davidreband <david.reband@gmail.com>
2018-02-20 10:45:42 +0100

    Test6

--- app/src/main/java/com/example/administrator/shoppinglisthq/MainActivity.java
+++ app/src/main/java/com/example/administrator/shoppinglisthq/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private  String aaa ="hdsgsdghjjlhklhljlhjh";
+    private  String aaa ="hdsgsdhklhljlhjh";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
